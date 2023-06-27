a1031f25a01bd2fabd4bcfb1fb6cce4737c7a7d8
Hwan <58416696+Hwanyu@users.noreply.github.com>
2023-06-27 11:53:25 +0900

    Add files via upload

---
+++ 19년도 기말 발표과제/단과대소개과제 - 복사본.py
@@ -0,0 +1,527 @@
+from tkinter import*
+from tkinter.filedialog import*
+
+
+##함수선언부분##
+def func_open_home():
+    wall= PhotoImage(file="MOKWON.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+    
+def func_open_a():
+    wall= PhotoImage(file="MU1.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+
+def func_open_b1():
+    wall= PhotoImage(file="MU2.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+
+def func_open_b2():
+    wall= PhotoImage(file="MU3.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_b3():
+    wall= PhotoImage(file="MU4.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_b4():
+    wall= PhotoImage(file="MU5.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_b5():
+    wall= PhotoImage(file="MU6.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_b6():
+    wall= PhotoImage(file="MU7.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+    
+def func_open_b7():
+    wall= PhotoImage(file="MU8.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_b8():
+    wall= PhotoImage(file="MU9.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_c1():
+    wall= PhotoImage(file="MU10.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_c2():
+    wall= PhotoImage(file="MU11.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_c3():
+    wall= PhotoImage(file="MU12.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_c4():
+    wall= PhotoImage(file="MU13.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_c5():
+    wall= PhotoImage(file="MU14.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_c6():
+    wall= PhotoImage(file="MU15.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_c7():
+    wall= PhotoImage(file="MU16.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_d1():
+    wall= PhotoImage(file="MU17.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_d2():
+    wall= PhotoImage(file="MU18.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_d3():
+    wall= PhotoImage(file="MU19.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_d4():
+    wall= PhotoImage(file="MU20.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_d5():
+    wall= PhotoImage(file="MU21.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_d6():
+    wall= PhotoImage(file="MU22.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_d7():
+    wall= PhotoImage(file="MU23.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_d8():
+    wall= PhotoImage(file="MU24.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_d9():
+    wall= PhotoImage(file="MU25.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_d10():
+    wall= PhotoImage(file="MU26.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_e1():
+    wall= PhotoImage(file="MU27.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+def func_open_e2():
+    wall= PhotoImage(file="MU28.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_e3():
+    wall= PhotoImage(file="MU29.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_e4():
+    wall= PhotoImage(file="MU30.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_e5():
+    wall= PhotoImage(file="MU31.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_e6():
+    wall= PhotoImage(file="MU32.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_e7():
+    wall= PhotoImage(file="MU33.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_f1():
+    wall= PhotoImage(file="MU34.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_f2():
+    wall= PhotoImage(file="MU35.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_f3():
+    wall= PhotoImage(file="MU36.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_f4():
+    wall= PhotoImage(file="MU37.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_f5():
+    wall= PhotoImage(file="MU38.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_g2():
+    wall= PhotoImage(file="MU40.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_g3():
+    wall= PhotoImage(file="MU41.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_g4():
+    wall= PhotoImage(file="MU42.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_g5():
+    wall= PhotoImage(file="MU43.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_g6():
+    wall= PhotoImage(file="MU44.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_h1():
+    wall= PhotoImage(file="MU45.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_h2():
+    wall= PhotoImage(file="MU46.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_h3():
+    wall= PhotoImage(file="MU47.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_h4():
+    wall= PhotoImage(file="MU48.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_h5():
+    wall= PhotoImage(file="MU49.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_h6():
+    wall= PhotoImage(file="MU50.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_h7():
+    wall= PhotoImage(file="MU51.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+
+def func_open_i1():
+    wall= PhotoImage(file="MU52.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+def func_open_i2():
+    wall= PhotoImage(file="MU53.gif")
+    wall_label =Label(image=wall)
+    wall_label.place(x=-2,y=-2)
+    window.mainloop()
+
+
+##메인코드부분##
+window=Tk()
+window.geometry("750x500")
+window.title("목원대학교 소개페이지")
+
+
+mainMenu=Menu(window)
+window.config(menu=mainMenu)
+photo=PhotoImage(file="MOKWON.GIF")
+pLabel=Label(window,image=photo)
+pLabel.pack(expand=1,anchor=CENTER)
+
+
+imageMenu=Menu(mainMenu)
+mainMenu.add_cascade(label="HOME",command=func_open_home)
+
+imageMenu=Menu(mainMenu)
+mainMenu.add_cascade(label="신학대학",menu=imageMenu)
+imageMenu.add_command(label="신학전공",command=func_open_a)  ##MU1
+
+
+image1Menu=Menu(mainMenu)
+mainMenu.add_cascade(label="공과대학",menu=image1Menu)
+image1Menu.add_command(label="건축학부",command=func_open_b1)  ##MU2
+image1Menu.add_separator()
+image1Menu.add_command(label="도시공학과",command=func_open_b2)
+image1Menu.add_separator()
+image1Menu.add_command(label="융합컴퓨터미디어학부",command=func_open_b3)
+image1Menu.add_separator()
+image1Menu.add_command(label="전자공학과",command=func_open_b4)
+image1Menu.add_separator()
+image1Menu.add_command(label="정보통신융합공학과",command=func_open_b5)
+image1Menu.add_separator()
+image1Menu.add_command(label="신소재화학공학과",command=func_open_b6)
+image1Menu.add_separator()
+image1Menu.add_command(label="지능로봇공학과",command=func_open_b7)
+image1Menu.add_separator()
+image1Menu.add_command(label="도시환경화학공학과",command=func_open_b8)  ###8
+
+image2Menu=Menu(mainMenu)
+mainMenu.add_cascade(label="인문대학",menu=image2Menu)
+image2Menu.add_command(label="국어국문학과",command=func_open_c1)##MU10
+image2Menu.add_separator()
+image2Menu.add_command(label="영어영문학과",command=func_open_c2)
+image2Menu.add_separator()
+image2Menu.add_command(label="독일언어문화학과",command=func_open_c3)
+image2Menu.add_separator()
+image2Menu.add_command(label="프랑스문화학과",command=func_open_c4)
+image2Menu.add_separator()
+image2Menu.add_command(label="역사학과",command=func_open_c5)
+image2Menu.add_separator()
+image2Menu.add_command(label="사회복지학과",command=func_open_c6)
+image2Menu.add_separator()
+image2Menu.add_command(label="국제국문학과",command=func_open_c7)   ##7
+image2Menu.add_separator()
+
+image3Menu=Menu(mainMenu)
+mainMenu.add_cascade(label="사회과학대학",menu=image3Menu)
+image3Menu.add_command(label="경영학과",command=func_open_d1)##MU17
+image3Menu.add_separator()
+image3Menu.add_command(label="서비스경영학부",command=func_open_d2)
+image3Menu.add_separator()
+image3Menu.add_command(label="금융보험부동산학과",command=func_open_d3)
+image3Menu.add_separator()
+image3Menu.add_command(label="무역학과",command=func_open_d4)
+image3Menu.add_separator()
+image3Menu.add_command(label="경제학과",command=func_open_d5)
+image3Menu.add_separator()
+image3Menu.add_command(label="마케팅정보컨설팅학과",command=func_open_d6)
+image3Menu.add_separator()
+image3Menu.add_command(label="중국학과",command=func_open_d7)
+image3Menu.add_separator()
+image3Menu.add_command(label="광고홍보언론학과",command=func_open_d8)
+image3Menu.add_separator()
+image3Menu.add_command(label="행정학과",command=func_open_d9)
+image3Menu.add_separator()
+image3Menu.add_command(label="경찰법학과",command=func_open_d10)   ##10
+
+image4Menu=Menu(mainMenu)
+mainMenu.add_cascade(label="테크노과학대학",menu=image4Menu)
+image4Menu.add_command(label="수학과",command=func_open_e1)##MU27
+image4Menu.add_separator()
+image4Menu.add_command(label="생의약화장품학부",command=func_open_e2)
+image4Menu.add_separator()
+image4Menu.add_command(label="의생명보건학부",command=func_open_e3)
+image4Menu.add_separator()
+image4Menu.add_command(label="지식재산학과",command=func_open_e4)
+image4Menu.add_separator()
+image4Menu.add_command(label="스포츠건강관리학과",command=func_open_e5)
+image4Menu.add_separator()
+image4Menu.add_command(label="미생물나노소재학과",command=func_open_e6)
+image4Menu.add_separator()
+image4Menu.add_command(label="소방안전관리학과",command=func_open_e7)   ##7
+image4Menu.add_separator()
+
+image5Menu=Menu(mainMenu)
+mainMenu.add_cascade(label="음악대학",menu=image5Menu)
+image5Menu.add_command(label="성악,뮤지컬학과",command=func_open_f1)##MU34
+image5Menu.add_separator()
+image5Menu.add_command(label="작곡,재즈학부",command=func_open_f2)
+image5Menu.add_separator()
+image5Menu.add_command(label="관현악학부",command=func_open_f3)
+image5Menu.add_separator()
+image5Menu.add_command(label="피아노과",command=func_open_f4)
+image5Menu.add_separator()
+image5Menu.add_command(label="국악과",command=func_open_f5)   ##5
+image5Menu.add_separator()
+
+image6Menu=Menu(mainMenu)
+mainMenu.add_cascade(label="미술디자인대학",menu=image6Menu)
+image6Menu.add_command(label="산업디자인학과",command=func_open_g2)##MU40
+image6Menu.add_separator()
+image6Menu.add_command(label="도자디자인학과",command=func_open_g3)
+image6Menu.add_separator()
+image6Menu.add_command(label="만화,애니메이션학과",command=func_open_g4)
+image6Menu.add_separator()
+image6Menu.add_command(label="시각디자인학과",command=func_open_g5)
+image6Menu.add_separator()
+image6Menu.add_command(label="섬유패션디자인학과",command=func_open_g6)   ##5
+image6Menu.add_separator()
+
+image7Menu=Menu(mainMenu)
+mainMenu.add_cascade(label="사범대학",menu=image7Menu)
+image7Menu.add_command(label="국어교육과",command=func_open_h1)##MU45
+image7Menu.add_separator()
+image7Menu.add_command(label="영어교육과",command=func_open_h2)
+image7Menu.add_separator()
+image7Menu.add_command(label="수학교육과",command=func_open_h3)
+image7Menu.add_separator()
+image7Menu.add_command(label="음악교육과",command=func_open_h4)
+image7Menu.add_separator()
+image7Menu.add_command(label="미술교육과",command=func_open_h5)
+image7Menu.add_separator()
+image7Menu.add_command(label="컴퓨터교육과",command=func_open_h6)
+image7Menu.add_separator()
+image7Menu.add_command(label="유아교육과",command=func_open_h7)   ##7
+image7Menu.add_separator()
+
+image8Menu=Menu(mainMenu)
+mainMenu.add_cascade(label="TV영화학부",menu=image8Menu)
+image8Menu.add_command(label="연기전공",command=func_open_i1)##MU52
+image8Menu.add_separator()
+image8Menu.add_command(label="TV영화전공",command=func_open_i2)  ##2
+image8Menu.add_separator()
+
+
+
+window.mainloop()
